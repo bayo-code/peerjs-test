f9b4b91cc82eb463f962ca1d22676a0953e13b66
bayo-code <hardebahyho@gmail.com>
2024-09-27 11:13:24 +0100

    Added some input

--- src/routes/client/+page.svelte
+++ src/routes/client/+page.svelte
@@ -1,23 +1,27 @@
 <script lang="ts">
 	import { Peer } from "peerjs"
-	import { onMount } from 'svelte';
 
 	let peer = new Peer("other")
 
 	let connected = false
+	let value: string = ""
 
-	onMount(() => {
+	function connectToUser() {
 		let getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 		getUserMedia({audio: true}, (stream) => {
-			let call = peer.call('waiter', stream)
+			let call = peer.call(value, stream)
 			console.log('Connected! Call: ', call);
 			call.on('stream', (remoteStream) => {
 				connected = true;
 				document.querySelector('#remote-audio').srcObject = remoteStream
 			})
 		})
-	})
+	}
 </script>
 
+<input type="text" bind:value />
+<button on:click={connectToUser}>
+	Connect
+</button>
 <h1>{connected ? 'Connected' : 'Connecting'}</h1>
 <audio id="remote-audio" controls autoplay></audio>
